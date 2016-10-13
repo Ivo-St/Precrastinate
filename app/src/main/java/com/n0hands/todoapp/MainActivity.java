e18fd12e5f7e2ddc1d40f7906bfe782555908c96
ivostoianov <ivostoianov94@gmail.com>
2016-10-13 10:01:49 +0300

    Add list view test activity for testing

--- app/src/main/java/com/n0hands/todoapp/MainActivity.java
+++ app/src/main/java/com/n0hands/todoapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.n0hands.todoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         testView = (TextView) findViewById(R.id.textView);
+        Log.d("aa","aa");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
     }
 
@@ -41,7 +44,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.load_game:
                 Toast.makeText(this, "Load Game", Toast.LENGTH_SHORT).show();
-                setContentView(R.layout.activity_login);
+                Intent intentLogin = new Intent(this, LoginActivity.class);
+                startActivity(intentLogin);
+                break;
+            case R.id.test_game:
+                Intent intentTest = new Intent(this, Test.class);
+                startActivity(intentTest);
                 break;
             default:
                 Toast.makeText(this, "Unsupported operation", Toast.LENGTH_SHORT).show();
